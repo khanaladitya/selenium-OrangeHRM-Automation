14574faa2e775c3a1a644e139e029f38e4750fee
ak <khanaladitya5@gmail.com>
2024-03-28 10:32:39 +0545

    updated config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
- # importing modules
+# importing modules
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.service import Service
@@ -7,30 +7,26 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import os
 from selenium.webdriver.common.action_chains import ActionChains
+
 baseurl = "https://opensource-demo.orangehrmlive.com"
 Gname = "Admin"
 Gpass = "admin123"
 EmpUname = "aditya47"
 EmpPass = "ak47@134"
 EmChangepass = "aditya12345"
+
 # browser configuration
 options = webdriver.ChromeOptions()
-# options = webdriver.EdgeOptions()
-# options = webdriver.FirefoxOptions()
 options.add_argument("--window-size=1920x1080")
 options.add_argument("--start-maximized")
 options.add_argument("--incognito")
 
 driver = webdriver.Chrome(options)
-# driver = webdriver.Edge(options)
-# driver = webdriver.Firefox(options)
-# create a Chrome WebDriver instance with the specified ChromeOptions
 
 
 def BrowserCall():
